7a6c6881e8abf0acb5c2e8994fd68f2feac704e6
JairSilva37 <jairrss@hotmail.com>
2019-02-28 16:16:25 -0300

    Formatando Campos de exibição

--- SalesWebMvc/Startup.cs
+++ SalesWebMvc/Startup.cs
@@ -13,6 +13,8 @@ using Microsoft.EntityFrameworkCore;
 using SalesWebMvc.Models;
 using SalesWebMvc.Data;
 using SalesWebMvc.Services;
+using Microsoft.AspNetCore.Localization;
+using System.Globalization;
 
 namespace SalesWebMvc
 {
@@ -51,6 +53,16 @@ namespace SalesWebMvc
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env,SeedingService seedingService)
         {
+            var enUS = new CultureInfo("en-US");
+            var localizationoptions = new RequestLocalizationOptions
+            {
+                DefaultRequestCulture = new RequestCulture(enUS),
+                SupportedCultures= new List<CultureInfo> { enUS},
+                SupportedUICultures= new List<CultureInfo> { enUS}
+            };
+
+            app.UseRequestLocalization(localizationoptions);
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
